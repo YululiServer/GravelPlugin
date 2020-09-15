7cb3c054c8c446fc50e50b2c16759de24e456a5e
kanasaki15 <kassi_0123@kassi-hp.info>
2020-09-15 18:55:32 +0900

    とりま？

---
+++ src/main/java/xyz/n7mn/dev/yululi/gravelplugin/GravelPluginListener.java
@@ -0,0 +1,25 @@
+package xyz.n7mn.dev.yululi.gravelplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class GravelPluginListener implements Listener {
+
+    @EventHandler
+    public void ee(PlayerCommandPreprocessEvent e){
+        if (e.getMessage().startsWith("/game-start")){
+
+            if (e.getPlayer().hasPermission("gravel.op")){
+
+
+
+                e.setCancelled(true);
+            }
+
+
+        }
+    }
+
+
+}
